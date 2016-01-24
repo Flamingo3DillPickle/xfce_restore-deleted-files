59b5fefa435a2aa1c7a97961b393d7c0aa0287ba
Francesco <ruggfrancesco@gmail.com>
2016-01-24 20:47:14 +0100

    Update restore_deleted_files.sh

--- restore_deleted_files.sh
+++ restore_deleted_files.sh
@@ -1,12 +1,13 @@
 #Add a shortcut from Xfce menu
 cd /home/$USER/.local/share/Trash/files/
 
-last_file=$(command ls -Art | tail -n 1 | tr '\n' ' ')
-trash_info=$(echo $last_file | sed -e 's/\(^.*\)/\1.trashinfo/' |tr '\n' ' ')
+last_file=$(command ls -Art | tail -n 1)
+trash_info=$(echo $last_file | sed -e 's/\(^.*\)/\1.trashinfo/')
 
 move_to=$(cat ../info/$trash_info | tail -n 2 | head -n 1 | awk -F"=" '{print $2}')
 
 #Debug info
-#echo $last_file $trash_info $move_to
+#echo "$last_file" "$trash_info" "$move_to"
 
-mv $last_file $move_to
+mv "$last_file" "$move_to"
+rm "../info/$trash_info"
