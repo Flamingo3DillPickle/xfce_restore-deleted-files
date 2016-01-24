38bec35650596e8b8c71ab2eb81d9497214e331b
Francesco Ruggeri <ruggfrancesco@gmail.com>
2016-01-24 17:53:12 +0100

    Initialing the repository

---
+++ restore_deleted_files.sh
@@ -0,0 +1,12 @@
+#Add a shortcut from Xfce menu
+cd /home/$USER/.local/share/Trash/files/
+
+last_file=$(command ls -Art | tail -n 1 | tr '\n' ' ')
+trash_info=$(echo $last_file | sed -e 's/\(^.*\)/\1.trashinfo/' |tr '\n' ' ')
+
+move_to=$(cat ../info/$trash_info | tail -n 2 | head -n 1 | awk -F"=" '{print $2}')
+
+#Debug info
+#echo $last_file $trash_info $move_to
+
+mv $last_file $move_to
